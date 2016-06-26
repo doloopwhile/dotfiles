3bd5b61469447640c554dc22ea1682d7a95831cb
OMOTO Kenji <doloopwhile@gmail.com>
2016-06-26 16:43:55 +0900

    updated

--- bin/time-signal.sh
+++ bin/time-signal.sh
@@ -1,16 +1,39 @@
 #!/bin/bash
+# while true; do
+#   if [ $(date +%M%S) = '0057' ]; then
+#     beep -f 440 -l 100
+#     sleep 0.9
+#     beep -f 440 -l 100
+#     sleep 0.9
+#     beep -f 440 -l 100
+#     sleep 0.9
+#     beep -f 880 -l 1000
+#   elif [ $(date +%M%S) = '3000' ]; then
+#     beep -f 880 -l 500
+#     sleep 1
+#   fi
+#   sleep 0.1
+# done
+
 while true; do
-  if [ $(date +%M%S) = '0057' ]; then
-    beep -f 440 -l 100
-    sleep 0.9
-    beep -f 440 -l 100
-    sleep 0.9
-    beep -f 440 -l 100
-    sleep 0.9
-    beep -f 880 -l 1000
+  f1=1100
+  f2=550
+  if [ $(date +%M%S) = '0000' ]; then
+    h=$(date +%I)
+    if [ $h -eq 0 ]; then
+      h=12
+    fi
+    for i in $(seq $h); do
+      beep -f $f1 -l 200
+      beep -f $f2 -l 200
+      sleep 0.6
+    done
+    sleep 1
   elif [ $(date +%M%S) = '3000' ]; then
-    beep -f 880 -l 500
+    beep -f $f1 -l 200
+    sleep 0.4
+    beep -f $f2 -l 400
     sleep 1
   fi
-  sleep 0.1
+  sleep 0.3
 done
