d9df9c47817907ca0de05338bbdf97266b41458e
OMOTO Kenji <doloopwhile@gmail.com>
2016-11-26 21:54:02 +0900

    updated

--- bash/git.sh
+++ bash/git.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #git関連
 export GIT_DISCOVERY_ACROSS_FILESYSTEM=true
-export PATH=$PATH:/usr/local/share/git-core/contrib/diff-highlight
+export PATH=$PATH:/usr/local/share/git-core/contrib/diff-highlight:/usr/share/doc/git/contrib/diff-highlight
 export PATH=$PATH:$HOME/.git-hooker/bin
 export GITHUB_USER=doloopwhile
 if which hub &> /dev/null; then
