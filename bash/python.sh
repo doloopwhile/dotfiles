c244ff8a3bff198201a86916b5aafec02cda42ea
OMOTO Kenji <doloopwhile@gmail.com>
2017-04-11 21:41:38 +0900

    updated

--- bash/python.sh
+++ bash/python.sh
@@ -3,6 +3,8 @@
 export PYTHONSTARTUP=~/.pythonstartup
 # export PIP_USER=1
 
+export PATH=~/.pyenv/bin/:$PATH
+
 if which pyenv &> /dev/null; then
   export PYENV_ROOT="$HOME/.pyenv"
   export PATH="$PYENV_ROOT/bin:$PATH"
