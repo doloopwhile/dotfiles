218c386ff7806ee845207a1e9bffc4d09f12a1e9
OMOTO Kenji <doloopwhile@gmail.com>
2017-09-09 11:11:19 +0900

    updated

--- bin/unar.sh
+++ bin/unar.sh
@@ -1,8 +1,10 @@
 #!/bin/sh
 for f in "$@"; do
+  notify-send -t 1000 "${f##*/} を展開しています"
   unar -r "$f"
   if [ $? -eq 0 ]; then
     notify-send "${f##*/} を展開しました"
+    trash "$f" 
   else
     notify-send "${f##*/} の展開に失敗しました"
   fi
