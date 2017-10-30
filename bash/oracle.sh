1d24da06cbea376fc5298c807a198ffa78997a8e
OMOTO Kenji <k-omoto@m3.com>
2017-10-30 14:21:21 +0900

    updated

--- bash/oracle.sh
+++ bash/oracle.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
-if [ "$(hostname)" = "m3-2015mac03.office.so-netm3.com" ]; then
-  # export ORACLE_HOME=$HOME/instantclient_11_2/
-  export OCI_DIR=$HOME/instantclient_11_2/
+ORACLE_HOME=$HOME/instantclient_11_2/
+
+if [ -d "$ORACLE_HOME" ]; then
+  export ORACLE_HOME
   export PATH="$PATH:$ORACLE_HOME"
-  # export DYLD_LIBRARY_PATH=$ORACLE_HOME:$DYLD_LIBRARY_PATH
+  export LD_LIBRARY_PATH=$ORACLE_HOME:$LD_LIBRARY_PATH
   export NLS_LANG=American_America.JA16EUCTILDE
-  # export TNS_ADMIN=$ORACLE_HOME/admin/network
+  export TNS_ADMIN=$ORACLE_HOME/admin/network
 fi
