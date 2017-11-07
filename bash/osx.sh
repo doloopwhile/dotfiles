e84c8a0c6b04592eaca2afc79a98cb9eec6898e7
OMOTO Kenji <k-omoto@m3.com>
2017-11-07 11:53:09 +0900

    updated

--- bash/osx.sh
+++ bash/osx.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
-if which brew &> /dev/null && [ -f "$(brew --prefix)/etc/bash_completion" ]; then
-  source "$(brew --prefix)/etc/bash_completion"
-fi
+if which brew &> /dev/null; then
+  if [ -f "$(brew --prefix)/etc/bash_completion" ]; then
+    . "$(brew --prefix)/etc/bash_completion"
+  fi
 
-## Enable GNU command in OSX
-if which gsed &> /dev/null; then
-  alias sed=gsed
+  ## Enable GNU command in OSX
+  if which gsed &> /dev/null; then
+    alias sed=gsed
+  fi
 fi
