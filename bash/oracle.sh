3e07d02b7e40abbd7e9be56705b8be24dba9b638
OMOTO Kenji <k-omoto@m3.com>
2019-01-22 12:05:55 +0900

    updated

--- bash/oracle.sh
+++ bash/oracle.sh
@@ -1,10 +1,6 @@
 #!/bin/bash
-# ORACLE_HOME=$HOME/instantclient_11_2/
-
-# if [ -d "$ORACLE_HOME" ]; then
-#   export ORACLE_HOME
-#   export PATH="$PATH:$ORACLE_HOME"
-#   export LD_LIBRARY_PATH=$ORACLE_HOME:$LD_LIBRARY_PATH
-#   export NLS_LANG=American_America.JA16EUCTILDE
-#   export TNS_ADMIN=$ORACLE_HOME/admin/network
-# fi
+export ORACLE_HOME="$HOME/instantclient_11_2/"
+export PATH="$PATH:$ORACLE_HOME"
+export LD_LIBRARY_PATH="$ORACLE_HOME:$LD_LIBRARY_PATH"
+export NLS_LANG=American_America.JA16EUCTILDE
+export TNS_ADMIN="$ORACLE_HOME/admin/network"
--- bash/python.sh
+++ bash/python.sh
@@ -10,6 +10,5 @@ export PYENV_ROOT="$HOME/.pyenv"
 export PATH="$PYENV_ROOT/bin:$PATH"
 
 if which pyenv &> /dev/null; then
-  export PYENV_ROOT="$HOME/.pyenv"
-  export PATH="$PYENV_ROOT/bin:$PATH"
+  eval "$(pyenv init -)"
 fi
