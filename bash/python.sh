e62c7bf78621963a074bf0bf52d9e6a2620cbd02
OMOTO Kenji <k-omoto@m3.com>
2019-02-01 18:27:26 +0900

    updated

--- bash/python.sh
+++ bash/python.sh
@@ -9,6 +9,6 @@ export PYTHONIOENCODING=utf-8
 export PYENV_ROOT="$HOME/.pyenv"
 export PATH="$PYENV_ROOT/bin:$PATH"
 
-if which pyenv &> /dev/null; then
-  eval "$(pyenv init -)"
-fi
+# if which pyenv &> /dev/null; then
+#   eval "$(pyenv init -)"
+# fi
